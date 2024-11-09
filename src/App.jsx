d63fe6184b4203cdadfb317a2dddf03793103eab
Shubham singh <rajputshubhamsingh593@gmail.com>
2024-11-10 00:37:50 +0530

    Todo  List App

--- src/App.jsx
+++ src/App.jsx
@@ -20,19 +20,19 @@ const getInitialToDos = () => JSON.parse(localStorage.getItem("todos")) || [];
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-l from-blue-600 to-emerald-400">
     <div className="bg-white shadow-lg rounded-3xl p-16">
       <div className=" flex flex-row">
-      <img className="w-14 h-12" src={no} alt="" />
-      <h1 className="text-3xl font-bold text-center text-gray-900 mb-6">TODO LIST</h1>
+      <img className="w-10 h-10" src={no} alt="" />
+      <h1 className="text-3xl font-bold text-center text-gray-900 mb-6 font-serif">Todo List</h1>
       </div>
-      <div className="mb-4 flex rounded-full">
+      <div className="mb-4 flex rounded">
         <input value={input} onChange={(e) => setInput(e.target.value)} type="text" placeholder="Add a new todo" className="flex-grow px-3 py-2 border rounded-l-lg focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-        <button onClick={addTodo} className="bg-blue-500 text-white px-4 py-2 rounded-r-lg hover:bg-blue-800 flex-grow">Add</button>
+        <button onClick={addTodo} className="bg-blue-500 text-white px-4 py-2 rounded-r-full hover:bg-blue-800 flex-grow">Add</button>
       </div>
       <ul className="space-y-2">
         {
           todos.map((todo) => (
             <li
               key={todo.id}
-              className="flex items-center p-3 rounded-lg bg-slate-100 border border-gray-200"
+              className="flex items-center p-3 rounded-lg bg-slate-100 border border-gray-200 rounded-full"
             >
             <input type="checkbox"
               checked={todo.completed}
@@ -44,12 +44,12 @@ const getInitialToDos = () => JSON.parse(localStorage.getItem("todos")) || [];
               )}
               className="mr-2 h-5 w-5 text-blue-600"
             />
-            <span className={`flex-grow ${todo.completed ? "line-through text-gray-500" : "text-gray-800"}`}>
+            <span className={ `flex-grow ${todo.completed ?  "line-through text-gray-500" : "text-gray-800"}`}>
               {todo.text}
             </span>
             <button
               onClick={() => setTodos(todos.filter((t) => t.id !== todo.id))}
-              className="ml-2 border-none p-2 rounded-lg bg-red-500 text-white hover:bg-red-600"
+              className="ml-2 border-none p-2 rounded-full bg-blue-500 text-white hover:bg-red-600"
             >
               Delete
             </button>
